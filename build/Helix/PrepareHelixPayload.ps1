dea33ddc9903e5c3fb2c98c2cb2c87d16f1241c7
Keith Mahoney <kmahone@microsoft.com>
2018-12-18 13:03:33 -0800

    fix preparehelixpayload.ps1

--- build/Helix/PrepareHelixPayload.ps1
+++ build/Helix/PrepareHelixPayload.ps1
@@ -21,8 +21,8 @@ Copy-Item "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\r
 New-Item -ItemType Directory -Force -Path "$payloadDir\.NETCoreApp2.1\"
 Copy-Item "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\lib\netcoreapp2.1\*" "$payloadDir\.NETCoreApp2.1\"
 Copy-Item "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\native\*" "$payloadDir\.NETCoreApp2.1\"
-Copy-Item "$nugetPackagesDir\MUXCustomBuildTasks.1.0.39-test\tools\$platform\WttLog.dll" $payloadDir
 
+Copy-Item "$nugetPackagesDir\MUXCustomBuildTasks.1.0.39-test2\tools\$platform\WttLog.dll" $payloadDir
 Copy-Item "$nugetPackagesDir\MUXCustomBuildTasks.1.0.39-test2\tools\x64\Listdlls.exe" $payloadDir
 Copy-Item "$nugetPackagesDir\MUXCustomBuildTasks.1.0.39-test2\tools\x64\handle.exe" $payloadDir
 
